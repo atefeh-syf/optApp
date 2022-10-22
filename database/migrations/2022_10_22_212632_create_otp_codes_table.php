5e50e84a0fc8e632d70748673650fc11cc61cc2d
atefeh-syf <atefe.s.77218@gmail.com>
2022-10-23 00:58:12 +0330

    otp_codes migrate

---
+++ database/migrations/2022_10_22_212632_create_otp_codes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOtpCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otp_codes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->string('code');
+            $table->timestamp('expired_at')->default(\Carbon\Carbon::now()->addMinutes(3));
+            $table->timestamp('used_at')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otp_codes');
+    }
+}
